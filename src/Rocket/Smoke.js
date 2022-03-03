e3b155ebf7a0d6026c610f6062109cf0a5c4b664
Jenny <jennykunte90@gmail.com>
2022-03-03 12:40:20 -0800

    First commit

--- src/Rocket/Smoke.js
+++ src/Rocket/Smoke.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Smoke.module.css';
 
-export default class App extends React.Component{
-    render(){
-        return(
-            <div className={styles.smoke}>
-                <div className={styles.video} autPlay loop src="/smoke.mov"/>
-            </div>
-        );
-    }
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.smoke}>
+        <video className={styles.video} autoPlay loop src="/smoke.mov" />
+      </div>
+    );
+  }
 }
